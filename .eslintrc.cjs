7e2c069095123788fe5c6708b9a1cbe3d85fd265
Kamil <48324735+kamilpixel@users.noreply.github.com>
2025-08-22 10:02:16 +0800

    Updates

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    extraFileExtensions: ['.vue'],
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-typescript/recommended',
+    'plugin:prettier/recommended',
+    'plugin:vitest/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'vitest'],
+  rules: {
+    semi: 'off',
+    '@typescript-eslint/semi': ['error', 'always'],
+    'prettier/prettier': ['error', { semi: true }],
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: "CallExpression[callee.name='it']",
+        message: 'Use `test()` instead of `it()` for consistency.',
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
