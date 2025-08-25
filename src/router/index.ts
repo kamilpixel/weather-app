ba734b55f7c24ff8336a30418f3c7c40b65429d5
Kamil <48324735+kamilpixel@users.noreply.github.com>
2025-08-25 18:16:33 +0800

    text fix base path issue

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
-const HomeView = () => import('@/views/HomeView.vue');
-const DetailView = () => import('@/views/DetailView.vue');
-const ProfileView = () => import('@/views/ProfileView.vue');
-const NotFoundView = () => import('@/views/NotFoundView.vue');
+const HomeView = () => import('../views/HomeView.vue');
+const DetailView = () => import('../views/DetailView.vue');
+const ProfileView = () => import('../views/ProfileView.vue');
+const NotFoundView = () => import('../views/NotFoundView.vue');
 
 const router = createRouter({
   history: createWebHashHistory(),
